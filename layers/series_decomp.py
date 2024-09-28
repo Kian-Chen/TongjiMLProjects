1ff7b5d269c397bd31bc8ddf1ec66c559fb09aea
Kian Chen <chinesechenqa@163.com>
2024-09-28 20:34:31 +0800

    add ModernTCN pipeline

---
+++ layers/series_decomp.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+from layers.moving_avg import moving_avg
+
+
+class series_decomp(nn.Module):
+    """
+    Series decomposition block
+    """
+    def __init__(self, kernel_size):
+        super(series_decomp, self).__init__()
+        self.moving_avg = moving_avg(kernel_size, stride=1)
+
+    def forward(self, x):
+        moving_mean = self.moving_avg(x)
+        res = x - moving_mean
+        return res, moving_mean
